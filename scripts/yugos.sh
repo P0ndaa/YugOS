917940e8d6e6134347b58f769a1a33f7585d0610
Antonio Pandza <antonio.pandza00@gmail.com>
2021-01-28 11:36:18 +0100

    update to autoinstall script

--- scripts/yugos.sh
+++ scripts/yugos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yay -S cava compton-tryone-git polybar
-yay -S alacritty cmus i3-gaps neovim ranger rofi
+yay -S alacritty cmus i3-gaps neovim ranger rofi zsh
 git clone https://github.com/P0ndaa/YugOS.git
 cd YugOS
 sudo cp -r fonts/* /usr/share/fonts/TTF/
@@ -9,3 +9,8 @@ cp -r config/* ~/.config/
 cp -r config/.vim ~
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd config
+mv -t ~ .zshrc .zprofile
+
