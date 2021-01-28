4fb8e8ee13469e1435e2be70121fd2a7528ccffc
Antonio Pandza <antonio.pandza00@gmail.com>
2021-01-28 10:40:19 +0100

    configs

--- scripts/yugos.sh
+++ scripts/yugos.sh
@@ -4,6 +4,7 @@ yay -S cava compton-tryone-git polybar
 yay -S alacritty cmus i3-gaps neovim ranger rofi
 git clone https://github.com/P0ndaa/YugOS.git
 cd YugOS
-cp config/* ~/.config/
-
-
+cp -r config/* ~/.config/
+cp -r config/.vim ~
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
