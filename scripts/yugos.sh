49fa37af91a2a6f289985f79e2c8d4cac4f23781
Antonio Pandza <antonio.pandza00@gmail.com>
2021-01-28 10:49:19 +0100

    configs

--- scripts/yugos.sh
+++ scripts/yugos.sh
@@ -4,6 +4,7 @@ yay -S cava compton-tryone-git polybar
 yay -S alacritty cmus i3-gaps neovim ranger rofi
 git clone https://github.com/P0ndaa/YugOS.git
 cd YugOS
+sudo cp -r fonts/* /usr/share/fonts/TTF/
 cp -r config/* ~/.config/
 cp -r config/.vim ~
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
